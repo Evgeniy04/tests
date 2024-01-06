6a85a09d47326e6084af24b3a25b46444c93a537
Evgeniy04 <56125646+Evgeniy04@users.noreply.github.com>
2024-01-06 16:13:57 +0700

    Update api.js
    Changing URL

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,4 @@
-export let url_api = "http://localhost/api/";
+export let url_api = "http://avradev.ru/api/";
 export let domain = "http://avradev.ru";
 
 export let mongo_db = "mongodb://127.0.0.1:27017";
